bac4e070e340eacd460c02e576c0e759149a2408
anandhaprakashs03 <anandhaprakaksh@karunya.edu.in>
2024-04-14 16:02:45 +0530

    Initil Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// App.js
+
 import React from 'react';
 import Login from './All Pages/login';
+import Signup from './All Pages/signup'; // Import the Signup component
 import InitialPage from './All Pages/initialPage';
-import BackgroundGlobe from './BackgroundGlobe';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -10,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<InitialPage />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} /> {/* Add route for the sign-up page */}
       </Routes>
     </BrowserRouter>
   );
